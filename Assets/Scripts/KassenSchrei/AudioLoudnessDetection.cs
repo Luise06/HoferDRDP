14cedcb6a1e76372a715e164777cf15f61e14b8f
theoroiroi <theo.roider06@htl-salzburg.ac.at>
2023-12-11 16:17:28 +0100

    .

---
+++ Assets/Scripts/KassenSchrei/AudioLoudnessDetection.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+
+public class AudioLoudnessDetection : MonoBehaviour
+
+{
+    public int sampleWindow = 128;
+    private AudioClip microphoneClip;
+
+    private void Start()
+    {
+        MicrophoneToAudioClip();
+    }
+    public void MicrophoneToAudioClip()
+    {
+        string microphoneName = Microphone.devices[0];
+        microphoneClip = Microphone.Start(microphoneName, true, 20, AudioSettings.outputSampleRate);
+    }
+
+    public float GetLoudnessFromMicrophone()
+    {
+        return GetLoudnessFromAudioClip(Microphone.GetPosition(Microphone.devices[0]), microphoneClip);
+    }
+    public float GetLoudnessFromAudioClip(int clipPosition, AudioClip clip)
+
+    {
+
+        int startPosition = clipPosition - sampleWindow;
+
+        float[] waveData = new float[sampleWindow];
+
+        clip.GetData(waveData, startPosition);
+
+
+
+        float totalLoudness = 0f;
+
+        for (int i = 0; i < sampleWindow; i++)
+
+        {
+
+            totalLoudness += Mathf.Abs(waveData[i]);
+
+        }
+
+
+
+        return totalLoudness / sampleWindow;
+
+    }
+
+}
+
