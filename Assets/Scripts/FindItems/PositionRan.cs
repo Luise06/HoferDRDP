d68b3450adec6f99fea6985f59cde1b444d29f86
Luise06 <lukno20006@gmail.com>
2024-05-06 17:25:23 +0200

    Anpassen

--- Assets/Scripts/FindItems/PositionRan.cs
+++ Assets/Scripts/FindItems/PositionRan.cs
@@ -26,79 +26,11 @@ public class PositionRan : MonoBehaviour
     int randomObjekt;
     int randomAbwandlung;
 
-    //public RandomPosition randomPosition;
-
     private void Awake()
     {
-        //RandomArrayausWahl();
         RandomPos();
     }
-    /*public void RandomArrayausWahl()
-    {
-        randomObjekt = Random.Range(0, 7);
-        randomAbwandlung = Random.Range(0, 6);
-        if (randomObjekt == 0)
-        {
-            speed[randomAbwandlung].SetActive(true);
-            Instantiate(speed[randomAbwandlung], randomPosition.spawnPoints[randomPosition.randomPosition]);
-        }
-        if (randomObjekt == 1)
-        {
-            cocos[randomAbwandlung].SetActive(true);
-            Instantiate(cocos[randomAbwandlung], randomPosition.spawnPoints[randomPosition.randomPosition]);
-        }
-        if (randomObjekt == 2)
-        {
-            disco[randomAbwandlung].SetActive(true);
-            Instantiate(disco[randomAbwandlung], randomPosition.spawnPoints[randomPosition.randomPosition]);
-        }
-        /*if (randomObjekt == 3)
-        {
-            dolphys[randomAbwandlung].SetActive(true);
-            Instantiate(dolphys[randomAbwandlung], randomPosition.spawnPoints[randomPosition.randomPosition]);
-        }
-        if (randomObjekt == 4)
-        {
-            doppelkekse[randomAbwandlung].SetActive(true);
-            Instantiate(doppelkekse[randomAbwandlung], randomPosition.spawnPoints[randomPosition.randomPosition]);
-        }
-        if (randomObjekt == 5)
-        {
-            eisteeBlau[randomAbwandlung].SetActive(true);
-            Instantiate(eisteeBlau[randomAbwandlung], randomPosition.spawnPoints[randomPosition.randomPosition]);
-        }
-        if (randomObjekt == 6)
-        {
-            eisteeGruen[randomAbwandlung].SetActive(true);
-            Instantiate(eisteeGruen[randomAbwandlung], randomPosition.spawnPoints[randomPosition.randomPosition]);
-        }
-        /*if (randomObjekt == 7)
-        {
-            milch[randomAbwandlung].SetActive(true);
-            Instantiate(milch[randomAbwandlung], randomPosition.spawnPoints[randomPosition.randomPosition]);
-        }
-        if (randomObjekt == 8)
-        {
-            flyingPowerBlau[randomAbwandlung].SetActive(true);
-            Instantiate(flyingPowerBlau[randomAbwandlung], randomPosition.spawnPoints[randomPosition.randomPosition]);
-        }
-        if (randomObjekt == 9)
-        {
-            flyingPowerRosa[randomAbwandlung].SetActive(true);
-            Instantiate(flyingPowerRosa[randomAbwandlung], randomPosition.spawnPoints[randomPosition.randomPosition]);
-        }
-        if (randomObjekt == 10)
-        {
-            flyingPowerRot[randomAbwandlung].SetActive(true);
-            Instantiate(flyingPowerRot[randomAbwandlung], randomPosition.spawnPoints[randomPosition.randomPosition]);
-        }
-        if (randomObjekt == 7)
-        {
-            pizzaschiffchen[randomAbwandlung].SetActive(true);
-            Instantiate(pizzaschiffchen[randomAbwandlung], randomPosition.spawnPoints[randomPosition.randomPosition]);
-        }
-
-    }*/
+    
 
     public void RandomPos()
     {
@@ -173,9 +105,7 @@ public class PositionRan : MonoBehaviour
         {
             if (richtigePos != i)
             {
-
                 Instantiate(richtigeObjects[randomObjekt], transformList[i]);
-
             }
         }
     }
