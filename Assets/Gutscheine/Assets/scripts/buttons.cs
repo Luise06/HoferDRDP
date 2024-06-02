be7c430d78a161ee1321981ea33fbdb0d218fb8a
GlendaLP <glenda.peralta05@htl-salzburg.ac.at>
2024-06-03 01:18:36 +0200

    efafafay

--- Assets/Gutscheine/Assets/scripts/buttons.cs
+++ Assets/Gutscheine/Assets/scripts/buttons.cs
@@ -6,6 +6,9 @@ using UnityEngine.UI;
 public class buttons : MonoBehaviour
 {
 
+    public List<Button> gutscheinListe = new List<Button>();
+    
+
     public GameObject hideContent;
     public GameObject rotscan, gelbscan, blauscan, orangescan;
     public Button rot01, rot02, gelb01, gelb02, blau01, blau02, orange01, orange02;
@@ -17,7 +20,29 @@ public class buttons : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
+        for (int i = 0; i < gutscheinListe.Count; i++)
+        {
+            string gutscheinName = "GutscheinNR" + i;
+            int isSaved = PlayerPrefs.GetInt(gutscheinName);
+
+            if (isSaved == 1)
+            {
+                //gutscheinListe[i].SetActive(true);
+                Debug.Log("Gutschein wird angezeigt");
+                gutscheinListe[i].interactable = true;
+            }
+            else
+            {
+                gutscheinListe[i].interactable = false;
+
+            }
+
+
+
+
+        }
+
         rotscan.SetActive(false);
         blauscan.SetActive(false);
         orangescan.SetActive(false);
@@ -34,16 +59,32 @@ public class buttons : MonoBehaviour
     {
         if (rotscan)
         {
+            
+
             rotscan.SetActive(false);
             hideContent.SetActive(true);
-            rot01.interactable = false;
-            rot02.interactable = false;
+            //rot01.interactable = false;
+            // rot02.interactable = false;
+            
+
         }
 
         if (blauscan)
         {
+            string gutscheinName = "GutscheinNR" + 4;
+            string gutscheinName02 = "GutscheinNR" + 5;
+            int isSaved = PlayerPrefs.GetInt(gutscheinName);
+
+
+
             blauscan.SetActive(false);
             hideContent.SetActive(true);
+            blau01.interactable = false;
+
+            PlayerPrefs.SetInt(gutscheinName, 0);
+            PlayerPrefs.SetInt(gutscheinName02, 0);
+            PlayerPrefs.Save();
+
         }
 
         if (gelbscan)
